2700b1dcf2b2d5fbbee53aa270b7920041f6dac4
Kehinde <abdurraufsalahudeen63@gmail.com>
2023-03-27 17:59:05 -0700

    corrections

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string.
- * @str: Pointer to a string.
- * Return: void.
+ * puts2 - prints every other character of a string
+ * @str: string to print
  */
 void puts2(char *str)
 {
@@ -13,5 +12,6 @@ void puts2(char *str)
 	{
 		_putchar(str[i]);
 	}
+
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcpy - copies the string pointed to by src to the buffer pointed to by dest
+ * _strcpy - copy the string pointed to by src to the buffer pointed to by dest
  * @dest: buffer to copy the string to
  * @src: string to copy from
  *
