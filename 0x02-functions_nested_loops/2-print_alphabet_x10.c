771da0878a1bdbdad135b4275ad339451b455254
Kehinde <abdurraufsalahudeen63@gmail.com>
2023-03-28 22:28:33 -0700

    6-abs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,13 @@
 
 /**
  * print-alphabet_x10 - prints all alphabets 10 times in lowercase
- * followed by a new line
+ * Return: On success 1
  */
 void print_alphabet_x10(void)
 {
 	char letter = 'a';
 	int i, j;
-	
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 26; j++)
