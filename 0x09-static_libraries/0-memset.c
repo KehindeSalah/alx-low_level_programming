97e2da5f55d0dac2fa5984e18cd12eedb8b26744
Kehinde <abdurraufsalahudeen63@gmail.com>
2023-04-07 14:45:39 -0700

    libmy.a

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fill an amount of memory with a certain value
+ * @s: starting address of the memory
+ * @b: the desired value
+ * @n: number of bytes to be changed
+ *
+ * Return: changed array with new value for n bytes
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen -tells us the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
